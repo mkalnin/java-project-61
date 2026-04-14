595ed981e10ccd7339444dc6eef60f5323a8a417
kalnin.ma <kalnin.ma@gazprom-neft.local>
2026-04-14 11:09:34 +0300

    Update versions plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.github.ben-manes.versions") version "0.53.0"
 }
 
 group = "hexlet.code"
@@ -7,14 +8,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    gradlePluginPortal()
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    runtimeOnly("com.github.ben-manes:gradle-versions-plugin:0.11.1")
 }
 
 tasks.test {
